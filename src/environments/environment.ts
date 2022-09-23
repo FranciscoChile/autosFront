8e0ba1e46644bc49d47c1e2ed1269a2089c7794b
Tavo <gustavoalvarez@femto.cl>
2022-09-23 00:18:42 -0300

    Change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api'
-  // apiUrl: 'https://mube-backend.herokuapp.com/api/cars'
+  // apiUrl: 'https://mube-backend.herokuapp.com/api'
 };
 
 /*
